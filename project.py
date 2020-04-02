e246108be9adb8912a5686736f99e1f2bfb86d04
Nonthakorn <32518286+Nonthakorn@users.noreply.github.com>
2020-04-02 21:37:46 +0700

    Update project.py

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 import pygame
-
-
+import tkinter as tk
+from tkinter import ttk
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
@@ -8,9 +8,10 @@ RED = (255, 0, 0)
 
 pygame.init()
 
+
 size = (509, 600)
 screen = pygame.display.set_mode(size)
-
+tkinput_1 = True
 pygame.display.set_caption("Test")
 
 walkRight = [pygame.image.load('Game\R1.png')]
@@ -32,7 +33,6 @@ jumpCount = 10
 left = False
 right = False
 walkCount = 0
-
 def redrawGameWindow():
     global walkCount
         
@@ -48,8 +48,8 @@ def redrawGameWindow():
     #pygame.display.flip()
     #screen.fill(255,255,255)
     #pygame.draw.rect(screen, (0, 0, 0), (x, y, width+1, height+1))
-    pygame.display.update()
 
+    pygame.display.update()
 run = True
 while run:
     pygame.time.delay(100)
@@ -89,10 +89,26 @@ while run:
             y += vel+margin
             left = False
             right = True
-
     redrawGameWindow()
+
+    window = tk.Tk()
+    window.title("Python Tkinter Text Box")
+    window.minsize(600, 400)
     #clock.tick(60)
-    
+    def clickMe():
+        label.configure(text='Hello ' + name.get())
+        print(name.get())
+        return name.get()
+
+    label = ttk.Label(window, text="Enter Your Name")
+    label.grid(column=0, row=0)
+    name = tk.StringVar()
+    nameEntered = ttk.Entry(window, width=15, textvariable=name)
+    nameEntered.grid(column=0, row=1)
+    button = ttk.Button(window, text="Click Me", command=clickMe)
+    button.grid(column=0, row=2)
+    window.mainloop()
+    #mainloop()
     #screen.fill(0,0,0)
     #pygame.draw.rect(screen, (255, 255, 255), (x, y, width, height))
     #pygame.display.update()
