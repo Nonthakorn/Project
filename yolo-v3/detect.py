79a9936161cf8be4cd6071216d833f773915f3e5
Chalermwong Jumnongpho <32519529+Justdoitlast@users.noreply.github.com>
2020-11-12 15:50:05 +0700

    add detect

--- yolo-v3/detect.py
+++ yolo-v3/detect.py
@@ -12,9 +12,11 @@ Example:
 Note that only one video can be processed at one run.
 """
 
+import pygame
 import tensorflow as tf
 import sys
 import cv2
+import numpy
 
 from yolo_v3 import Yolo_v3
 from utils import load_images, load_class_names, draw_boxes, draw_frame
@@ -24,7 +26,8 @@ tf.compat.v1.disable_eager_execution()
 _MODEL_SIZE = (416, 416)
 _CLASS_NAMES_FILE = './data/labels/obj.names'
 _MAX_OUTPUT_SIZE = 20
-
+pygame.init()
+screen = pygame.display.set_mode((800,800))
 
 def main(type, iou_threshold, confidence_threshold, input_names):
     class_names = load_class_names(_CLASS_NAMES_FILE)
@@ -35,66 +38,7 @@ def main(type, iou_threshold, confidence_threshold, input_names):
                     iou_threshold=iou_threshold,
                     confidence_threshold=confidence_threshold)
 
-    if type == 'images':
-        batch_size = len(input_names)
-        batch = load_images(input_names, model_size=_MODEL_SIZE)
-        inputs = tf.compat.v1.placeholder(tf.float32, [batch_size, *_MODEL_SIZE, 3])
-        detections = model(inputs, training=False)
-        saver = tf.compat.v1.train.Saver(tf.compat.v1.global_variables(scope='yolo_v3_model'))
-
-        with tf.compat.v1.Session() as sess:
-            saver.restore(sess, './weights/model.ckpt')
-            detection_result = sess.run(detections, feed_dict={inputs: batch})
-
-        draw_boxes(input_names, detection_result, class_names, _MODEL_SIZE)
-        #print(build_boxes)
-        print('Detections have been saved successfully.')
-
-    elif type == 'video':
-        inputs = tf.compat.v1.placeholder(tf.float32, [1, *_MODEL_SIZE, 3])
-        detections = model(inputs, training=False)
-        saver = tf.compat.v1.train.Saver(tf.compat.v1.global_variables(scope='yolo_v3_model'))
-
-        with tf.compat.v1.Session() as sess:
-            saver.restore(sess, './weights/model.ckpt')
-
-            win_name = 'Video detection'
-            cv2.namedWindow(win_name)
-            cap = cv2.VideoCapture(input_names[0])
-            frame_size = (cap.get(cv2.CAP_PROP_FRAME_WIDTH),
-                          cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-            fourcc = cv2.VideoWriter_fourcc(*'X264')
-            fps = cap.get(cv2.CAP_PROP_FPS)
-            out = cv2.VideoWriter('./detections/detections.mp4', fourcc, fps,
-                                  (int(frame_size[0]), int(frame_size[1])))
-
-            try:
-                while True:
-                    ret, frame = cap.read()
-                    if not ret:
-                        break
-                    resized_frame = cv2.resize(frame, dsize=_MODEL_SIZE[::-1],
-                                               interpolation=cv2.INTER_NEAREST)
-                    detection_result = sess.run(detections,
-                                                feed_dict={inputs: [resized_frame]})
-
-                    draw_frame(frame, frame_size, detection_result,
-                               class_names, _MODEL_SIZE)
-
-                    cv2.imshow(win_name, frame)
-
-                    key = cv2.waitKey(1) & 0xFF
-
-                    if key == ord('q'):
-                        break
-
-                    out.write(frame)
-            finally:
-                cv2.destroyAllWindows()
-                cap.release()
-                print('Detections have been saved successfully.')
-
-    elif type == 'webcam':
+    if type == 'webcam':
         inputs = tf.compat.v1.placeholder(tf.float32, [1, *_MODEL_SIZE, 3])
         detections = model(inputs, training=False)
         saver = tf.compat.v1.train.Saver(tf.compat.v1.global_variables(scope='yolo_v3_model'))
@@ -103,39 +47,52 @@ def main(type, iou_threshold, confidence_threshold, input_names):
             saver.restore(sess, './weights/model.ckpt')
 
             win_name = 'Webcam detection'
-            cv2.namedWindow(win_name)
+            # cv2.namedWindow(win_name)
+            
             cap = cv2.VideoCapture(0)
+            cap.set(3,640)
+            cap.set(4,480)
             frame_size = (cap.get(cv2.CAP_PROP_FRAME_WIDTH),
                           cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
             fourcc = cv2.VideoWriter_fourcc(*'X264')
             fps = cap.get(cv2.CAP_PROP_FPS)
             out = cv2.VideoWriter('./detections/detections.mp4', fourcc, fps,
                                   (int(frame_size[0]), int(frame_size[1])))
-
+            
+            screen = pygame.display.set_mode((1300,800))
             try:
                 while True:
+                    screen.fill((255,55,210))
+                    for event in pygame.event.get():  # User did something
+                        if event.type == pygame.QUIT:  # If user clicked close
+                            break  # Flag that we are done so we exit this loop
+                        if event.type == pygame.KEYDOWN:  # If user clicked close
+                            if event.key == pygame.K_q:
+                                break
                     ret, frame = cap.read()
                     if not ret:
                         break
+                    
+                    frame = frame.swapaxes(0, 1)
                     resized_frame = cv2.resize(frame, dsize=_MODEL_SIZE[::-1],
                                                interpolation=cv2.INTER_NEAREST)
                     detection_result = sess.run(detections,
                                                 feed_dict={inputs: [resized_frame]})
-
+                    framenew=numpy.rot90(frame)
+                    framenew=pygame.surfarray.make_surface(frame)
                     draw_frame(frame, frame_size, detection_result,
                                class_names, _MODEL_SIZE)
 
-                    cv2.imshow(win_name, frame)
-
+                    screen.blit(framenew,(0,0))
                     key = cv2.waitKey(1) & 0xFF
 
                     if key == ord('q'):
                         break
-
-                    out.write(frame)
+                    pygame.display.flip()
+                    # out.write(frame)
             finally:
                 cv2.destroyAllWindows()
-                cap.release()
+                # cap.release()
                 print('Detections have been saved successfully.')
 
     else:
@@ -143,4 +100,4 @@ def main(type, iou_threshold, confidence_threshold, input_names):
 
 
 if __name__ == '__main__':
-    main(sys.argv[1], float(sys.argv[2]), float(sys.argv[3]), sys.argv[4:])
+    main('webcam', 0.5, 0.5, sys.argv[4:])
