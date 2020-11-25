0bd823a62618d1d3364abfd2443f6773ae1c5b5d
Chalermwong Jumnongpho <32519529+Justdoitlast@users.noreply.github.com>
2020-11-26 03:48:59 +0700

    add more command

--- Game/runyolo.py
+++ Game/runyolo.py
@@ -3,6 +3,30 @@ import cv2
 import numpy as np
 import pandas as pd
 #Load YOLO
+def change_loop_command(lst):
+    check = [i for i,x in enumerate(lst) if x=='loop' or x=='end_num']
+    for i in range(0, len(check), 2):
+        count = 1
+        for j in range(check[i]+1, check[i+1]):
+            lst[j] = (count)
+            count+=1
+            
+    check2 = [i for i,x in enumerate(lst) if x=='end_num' or x=='end loop']
+    count = 0
+    for i in range(0, len(check2), 2):
+        for p in range(0, lst[check2[i]-2+count]):
+            for j in range(check2[i]+1+count, check2[i+1]+count):
+                lst.insert(check2[i+1]+count,lst[j])
+                count += 1
+        check = [i for i,x in enumerate(lst) if x=='loop' or x=='end_num']
+    for i in range(0, len(check), 2):
+        del lst[check[0]:check[1]+1]
+        check = [i for i,x in enumerate(lst) if x=='loop' or x=='end_num']
+
+    check_end_loop = [i for i,x in enumerate(lst) if x=='end loop']
+    for i in range(0, len(check_end_loop)):
+        lst.remove('end loop')
+    return lst
 def yolo():
     net = cv2.dnn.readNet("weights/yolov3_custom3_final.weights","weights/yolov3_custom3.cfg")
     classes = []
@@ -12,7 +36,7 @@ def yolo():
     outputlayers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
     colors= np.random.uniform(0,255,size=(len(classes),3))
     #loading image
-    img = cv2.imread("test_image/image.JPG")
+    img = cv2.imread("test_image/img_loop.JPG")
     img = cv2.resize(img,None,fx=0.4,fy=0.3)
     height,width,channels = img.shape
     blob = cv2.dnn.blobFromImage(img,0.00392,(416,416),(0,0,0),True,crop=False)
@@ -78,7 +102,7 @@ def yolo():
         "3": "turn left",
         "4": "loop",
         "5": "move",
-        "6": "start",
+        "6": "end_num",
         "7": "end loop",
         "8": "command3",
         "9": "if",
@@ -92,7 +116,7 @@ def yolo():
     for i in df['label']:
         # print(yoloclass[i])
         lst.append(yoloclass[i])
-    #print(lst)
+    lst = change_loop_command(lst)
     df2 = pd.DataFrame(lst, columns=['label'])
     df2.to_csv('input.txt', index=False, header=False)
     print("Sucessfully please check input.txt")
